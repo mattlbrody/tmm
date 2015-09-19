c2d70af6d8abbef101f081fffcf0a30b5ccfa21b
mattbrodzz <mattlbrody@gmail.com>
2015-09-18 17:06:04 -0700

    deployd truck locations now showing up on the map. need to figure out how to show images attached to each client

--- public/view1/view1.js
+++ public/view1/view1.js
@@ -12,115 +12,29 @@ var app = angular.module('myApp.view1', ['ngRoute', 'uiGmapgoogle-maps'])
 
 .controller('View1Ctrl', function($scope, position) {
     $scope.$on('mapInitialized', function(event, map) {
+      var i = 0;
       map.setCenter()
-      //service data goes here
-      console.log(position.user)
-      $scope.positions =[ [40.71, -74.21], [40.72, -74.20], [40.73, -74.19],
-      [40.74, -74.18], [40.75, -74.17], [40.76, -74.16], [40.77, -74.15]];
+      position.success(function(data) {
+        $scope.user = data;
+        $scope.positions = [];
+        $scope.icons = [];
+        for(i = 0; i < $scope.user.length; i++) {
+          $scope.id = $scope.user[i].id
+          $scope.icon = $scope.user[i].icon
+          $scope.lat = $scope.user[i].coord.latitude
+          $scope.lon = $scope.user[i].coord.longitude
+
+          if ($scope.user[i].open == true) {
+            function newPosition() {
+              $scope.positions.push([$scope.lat, $scope.lon])
+              $scope.icons.push($scope.icon)
+              console.log($scope.icon)
+            }
+            newPosition();
+
+            console.log($scope.icons)
+          }
+        }
+      });
   });
 })
-
-    
-
-    // dpd.users.get(function(users, error) {
-    //   users.forEach(function(user) {
-    //     id = users.id;
-    //     icon = users.icon;
-    //     coord = user.coord;
-    //   })
-    // })
-
-
-    // var mapOptions = {
-    //    zoom: 18,
-    //    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    //    styles: [
-    //       {
-    //         "featureType": "poi",
-    //         "stylers": [
-    //           { "visibility": "off" }
-    //         ]
-    //       }
-    //     ]
-    // };
-
-    // var map;
-
-    // function initialize() {
-
-      // if (navigator.geolocation) {
-      //      navigator.geolocation.getCurrentPosition(function (position) {
-      //          var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      //          map.setCenter(initialLocation);
-      //      });
-      //  }
-
-    //    var image = 'http://ladiesloot.com/wp-content/uploads/2015/05/smiley-face-1-4-15.png';
-    //      console.log(image)
-    //       var marker = new google.maps.Marker({
-    //         position: myLatlng1,
-    //         map: map,
-    //         title: 'Hello World!'
-    //       });
-          
-    //      marker.setMap(map);
-    //   }
-
-    //retrieves geoloacation data
-       // options = {
-       //    enableHighAccuracy: true,
-       //    timeout: 5000,
-       //    maximumAge: 0
-       //  };
-
-       //  //if successful, store in deployd
-       //  function success(pos) {
-       //    $scope.latitude = pos.coords.latitude;
-       //    $scope.longitude = pos.coords.longitude
-       //    $scope.crd = {
-       //      latitude,
-       //      longitude
-       //    };
-       //  console.log(latitude)
-
-       //  };
-       //  function error(err) {
-       //    console.warn('ERROR(' + err.code + '): ' + err.message);
-       //  };
-
-       //  navigator.geolocation.getCurrentPosition(success, error);
-
-
-
-    // $scope.map = {
-    //   center: {
-    //     latitude: 37.785991,
-    //     longitude: -122.397328
-    //   },
-    //   zoom: 18,
-    //   markers: [ 
-    //   {
-    //     id: '0',
-    //     icon: "http://codifyacademy.com/truckmouth/truckone.png",
-    //     coords: {
-    //       latitude: 37.785991,
-    //       longitude: -122.397328
-    //     } 
-    //   },
-    //   {
-    //     id: '1',
-    //     icon: "http://codifyacademy.com/truckmouth/trucktwo.png",
-    //     coords: {
-    //       latitude: 37.785779,
-    //       longitude: -122.399569
-    //     }
-    //   },
-    //   {
-    //     id: '2',
-    //     icon: "http://codifyacademy.com/truckmouth/truckthree.png",
-    //     coords: {
-    //       latitude: 37.787191,
-    //       longitude: -122.399027
-    //     }
-    //   }]
-    // }
--- public/view1/view1service.js
+++ public/view1/view1service.js
@@ -1,22 +1,9 @@
 app.factory('position', ['$http', function($http) {
-  // return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
-  //           .success(function(data) {
-  //             return data;
-  //           })
-  //           .error(function(err) {
-  //             return err;
-  //           });
-	return dpd.users.get(function(users, error) {
-	      users.forEach(function(user) {
-	        id = users.id;
-	        icon = users.icon;
-	        coord = user.coord;
-	      })
-	    })
-		.success(function(data) {
-		      return data;
-		    })
-		    .error(function(err) {
-		      return err;
-		 });
+  return $http.get('http://localhost:2403/users')
+            .success(function(data) {
+              return data;
+            })
+            .error(function(err) {
+              return err;
+          })
 }]);
